91fc62b548bbc2fc866b2580de7b26981854d5e7
Vaishali <vaishalithakur614@gmail.com>
2019-11-25 00:38:56 +0530

    Add files via upload

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList app v1.0.0. Its my first React App.</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
